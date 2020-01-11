c983a2c32df11a86dbb00416e8e9c026ffd23449
shajf <shajianfeng@antell.com.cn>
2020-01-11 20:29:45 +0800

    add new ips domains

--- diff.py
+++ diff.py
@@ -1,26 +1,25 @@
-#!/bin/python
+#!/usr/bin/python
 
 import sys
 
 result = set()
 
-if len(sys.argv) !=4:
-    print("usage:<pmain> <fpath1> <fpath2> <out>")
+if len(sys.argv) !=3:
+    print("usage:<pmain> <fpath1> <fpath2>")
     sys.exit(-1)
 
 fd1 = open(sys.argv[1],"r")
 fd2 = open(sys.argv[2],"r")
-fd3 = open(sys.argv[3],"w+")
 
 for line in fd1.readlines():
     result.add(line)
 
 for line in fd2.readlines():
-    result.add(line)
+    if line not in result:
+        print(line[:len(line)-1])
 
-fd3.writelines(result)
 
 
 fd1.close()
 fd2.close()
-fd3.close()
+
